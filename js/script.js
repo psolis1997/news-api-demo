ab382d9fae23e3cacdd35a1f9e94bba018c3a8bb
psolis1997 <patricia-anne@outlook.com>
2023-02-14 16:03:13 +1300

    Added Language Select

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const url = "https://gnews.io/api/v4/";
 // Get Inputs:
 const countrySelect = document.getElementById("countrySelect");
 const topicSelect = document.getElementById("topicSelect");
+const languageSelect = document.getElementById("languageSelect")
 const searchBtn = document.getElementById("searchSubmit");
 const searchInput = document.getElementById("searchInput");
 
@@ -25,21 +26,30 @@ function mainFunction(myKey) {
 
     getNews("en", "breaking-news");
 
+    $("#searchSubmit").click(function () {
+        console.log('search');
+        searchNews(searchInput.value);
+    });
+
     $("#countrySelect").change(function () {
-        filtered(countrySelect.value, topicSelect.value);
+        filtered(countrySelect.value, topicSelect.value, languageSelect.value);
     })
 
     $("#topicSelect").change(function () {
-        filtered(countrySelect.value, topicSelect.value);
+        filtered(countrySelect.value, topicSelect.value, languageSelect.value);
+    })
+
+    $("#languageSelect").change(function () {
+        filtered(countrySelect.value, topicSelect.value, languageSelect.value);
     })
 
     function filtered() {
-        getNews(countrySelect.value, topicSelect.value)
+        getNews(countrySelect.value, topicSelect.value, languageSelect.value)
     }
 
-    function getNews(country, topic) {
+    function getNews(country, topic, language) {
         $.ajax({
-            url: `${url}top-headlines?category=${topic}&lang=en&country=${country}&apikey=${key}`,
+            url: `${url}top-headlines?category=${topic}&lang=${language}&country=${country}&apikey=${key}`,
             type: "GET",
             data: "json",
     
@@ -64,13 +74,48 @@ function mainFunction(myKey) {
                                 <a class="text-danger" href="${story.url}" target="_blank"> View full article </a></p> 
                             </div>
                         </div>
-                    </div>;
-                    `
+                    </div>
+                    `;
                 }
             },
 
             error: function() {
                 console.log("Error - Could not get news");
+            }
+        })
+    }
+
+    function searchNews(search) {
+        $.ajax({
+            url: `${url}/search?q=${search}&lang=en&apikey=${key}`,
+            type: "GET",
+            data: "json",
+
+            success: function (newsData) {
+                let results = document.getElementById("results");
+                results.innerHTML = "";
+                for (let i = 0; i < newsData.articles.length; i++) {
+                    let story = newsData.articles[i];
+
+                    results.innerHTML += `
+                    <div class="col-lg-4 col-md-6 col-sm-1">
+                        <div class="card" mt-3>
+                            <img src="${story.image}" class="card-img-top" alt="news image"> 
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">${story.title}</h5> 
+                                <p class="card-text fw-light">${story.description}
+                                <br><br>Source:
+                                <a class="text-primary" href="${story.source.url}" target="_blank">${story.source.name}</a>
+                                <br><br>
+                                <a class="text-danger" href="${story.url}" target="_blank"> View full article </a></p> 
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                }
+            },
+            error: function () {
+                console.log("Could not do search");
             }
         })
     }
